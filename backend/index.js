29279fdb43bfc2b7c2cc71228fa6501ff2bfb8cb
Bhaskar Pujari <bhaskarpujari2006@gmail.com>
2025-09-05 22:38:18 +0530

    frontend url

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.send("Hello")
 })
-const allowedOrigins = ['https://lettercraft-eyb3.onrender.com', 'http://localhost:5173'];
+const allowedOrigins = ['https://letter-craft-beta.vercel.app/', 'http://localhost:5173'];
+
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -22,7 +23,6 @@ app.use(cors({
   },
   credentials: true
 }));
-
 
 app.use('/auth',authRouter)
 
